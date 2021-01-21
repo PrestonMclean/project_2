87c8b3d435793c526f21973f0c84a391385907b0
PrestonMclean <mclean_preston@hotmail.com>
2021-01-20 21:50:07 -0600

    random qestion generator
    added the random qestion generator function.

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,13 +1,37 @@
 <?php
 // Generate random questions
+function randomQuestionGenerator () {
+    // Get random numbers to add
+    $temp = [];
+    // get random numbers to add
+    switch ($_POST['dificulty']) {
+      case 'easy':
+        $temp['leftAdder'] = pow((rand(5, 95)),0);
+        $temp['rightAdder'] = pow((rand(5, 95)),0);
+        break;
+      case 'hard':
+        $temp['leftAdder'] = pow((rand(5, 95)),4);
+        $temp['rightAdder'] = pow((rand(5, 95)),4);
+        break;
+      case 'nomal':
+      default:
+        $temp['leftAdder'] = pow((rand(5, 95)),1);
+        $temp['rightAdder'] = pow((rand(5, 95)),1);
+        break;
+    }
+    // Calculate correct answer
+    $temp['correctAnswer'] = ($temp['leftAdder'] + $temp['rightAdder']);
+    $random1;
+    $random2;
+    // get random numbers that non zero and unique
+    do {
+      $random1 = rand(-(.3*$temp['correctAnswer']), 10);
+      $random2 = rand(-(.3*$temp['correctAnswer']), 10);
+    } while (($random1 == $random2) || ($random1 == 0) || ($random2 == 0));
 
-// Loop for required number of questions
-
-// Get random numbers to add
-
-// Calculate correct answer
-
-// Get incorrect answers within 10 numbers either way of correct answer
-// Make sure it is a unique answer
-
-// Add question and answer to questions array
+    // add the random numbers to the answer to get the wrong answers
+    $temp['firstIncorrectAnswer'] = ($temp['correctAnswer'] + $random1);
+    $temp['secondIncorrectAnswer'] = ($temp['correctAnswer'] + $random2);
+    // return the array
+    return $temp;
+}
